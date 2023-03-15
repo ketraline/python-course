c36ab45f69e14717145440f31976898f4c09fada
Rod <69829465+ketraline@users.noreply.github.com>
2023-03-15 10:28:11 +0100

    add code cause i forgot lmao

--- 2020/encoder.py
+++ 2020/encoder.py
@@ -1,1 +1,27 @@
+litery = "ABCDEFGHIJKLMNOPRSTUWXYZabcdefghijklmnoprstuwxyzĄĆĘŁÓŚŹŻąćęłóśźż"
 
+p1 = open("tekst_in.txt", "r")
+p2 = open("tekst_out.txt", "w")
+
+tekst_in = p1.read()
+tekst_out = ""
+
+for litera in tekst_in:
+  
+  if litera not in litery:
+    tekst_out = tekst_out + litera
+    
+  else:
+    indeks_1 = litery.find(litera)
+    indeks_2 = indeks_1 + 3
+  
+  if indeks_2 > 63:
+    indeks_2 = indeks_2 + 64
+    
+  tekst_out = tekst_out + litery[indeks_2]
+
+p2.write(tekst_out)
+print("zapisano plik")
+
+p1.close()
+p2.close()
