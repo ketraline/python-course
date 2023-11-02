45e250b96a2978f158a2084f3637ffd32ea05d84
Rod <69829465+ketraline@users.noreply.github.com>
2023-11-02 12:24:18 +0100

    wowza ! new update

---
+++ 2023/hangman/main.py
@@ -0,0 +1,38 @@
+import random
+difficulty = input("would you like 7 letter or 10 letter words? type 7 or 10 ")
+
+if difficulty == 10:
+    word = random.choice(open("2023/hangman/10letterwords.txt","r").read().split())
+else:
+    word = random.choice(open("2023/hangman/7letterwords.txt","r").read().split())
+    
+guessword = "_"*len(word)
+print(guessword)
+tries = int(len(word)/2+1)
+wrongletters = []
+
+while tries != 0:
+    letter = input("\ninput a letter: ")
+    if letter in word:
+        guessword = guessword[0:word.index(letter)] + letter + guessword[word.index(letter)+1:]
+        print(guessword)
+        print("remaining tries: ",tries)
+        print("wrong letters: ", wrongletters)
+        if guessword == word:
+            print("\nYOU WIN !!!!!!!!")
+            break
+    else:
+        if letter in wrongletters:
+            print(guessword)
+            print("you've already guessed this letter. try a different one")
+            print("remaining tries: ",tries)
+            print("wrong letters: ", wrongletters)
+        else:
+            print(guessword)
+            print("this letter isnt in the word")
+            tries -= 1
+            wrongletters += letter
+            print("remaining tries: ",tries)
+            print("wrong letters: ", wrongletters)
+    if tries == 0:
+        print("\nYOU LOSE")
---
+++ 2023/szachy/main.py
@@ -0,0 +1,38 @@
+# 1
+
+line = []
+full = []
+count = 0
+
+with open("2023/szachy/szachy_przyklad.txt", "r") as tekst:
+    for k in tekst:
+        if count%9<8:
+            line.append(k.strip())
+        else:
+            full.append(line.copy())
+            line.clear
+        count += 1
+
+print(*full[0], sep = "\n")
+
+for i in range(len(full)):
+    for j in range(8):
+        print(full[j][i], end = "    ")
+    print()
+
+# 1
+x = 0
+y = 0
+i = 0
+col0=[]
+while i < 150:
+    while x < 8 and y < 8:
+        col0 += full[i][x][y]
+        if col0 != '.':
+            col0.clear()
+            x += 1
+            y=0
+        else:
+            y += 1
+    i += 0 
+print(col0)
