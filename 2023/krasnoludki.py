e499d991123836d877e653ed7c088ec8087a4b8b
ketraline <ketraline@github.com>
2024-09-12 15:38:53 +0200

    translations and added tasks

--- 2023/krasnoludki.py
+++ 2023/krasnoludki.py
@@ -1,16 +1,32 @@
+# April - Exercise on string editing.
+
 a = "My jestesmy krasnoludki, hopsasa, hopsasa"
+
+# 1.1 - b equals 8 symbols starting from the letter j
 b = a[3:11]
-print("1.1 "+"Zmienna b ma wartość = ", b)
+print("1.1 "+"The variable b is equal = ", b)
+
+# 1.2 - c equals 7 of the last symbols of the variable a
 c = a[-7:]
-print("1.2 "+"Zmienna c ma wartość = ", c)
+print("1.2 "+"The variable c is equal = ", c)
+
+# 1.3 - d equals the word "my" all in lowercase and e equals the word "krasnoludki" all in uppercase
 d = a[0:2]
-print("1.3 "+"Zmienna d ma wartość = ", d.lower())
+print("1.3 "+"The variable d is equal = ", d.lower())
 e = a[12:23]
-print("1.3 "+"Zmienna e ma wartość = ", e.upper())
-print("1.4 "+"Zmienna e ma wartość = ", e.lower())
-print("1.5 "+"Zmienna d ma wartość = ", d.capitalize())
+print("1.3 "+"The variable e is equal = ", e.upper())
+
+# 1.4 Print e in all lowercase instead
+print("1.4 "+"The variable e is equal = ", e.lower())
+
+# 1.5 Print d with only the first letter in uppercase
+print("1.5 "+"The variable d is equal = ", d.capitalize())
+
+# 1.6 Print the sentence in variable a using the variables b,c,d,e
 print("1.6 "+d+" "+b+" "+e+", "+c+", "+c)
 
+# 2. We copy every third character of the variable I into the variable Wi, starting with a capital letter
+# We copy every 4th character of the N variable into the Wn variable, starting with a capital letter
 I="Rwdogrbnrimlnvsspponbnf"
 N="gClkjrvrtudrusjfdoveseh"
 
@@ -18,59 +34,25 @@ Wi = I[::3]
 Wn = N[1::4]
 print(f"2. {Wi}{Wn}")
 
-print(f"3. W zmiennej a wystepuje {a.count('s')} liter s oraz {a.count('a')} liter a")
+# 3. In the variable a from task 1, count the occurrences of the letter "s" and the letter "a"
+print(f"3. In the variable a there is {a.count('s')} letters 's' and {a.count('a')} letters 'a'")
 
+# 4. Remove all letters "x" from the variable p
 p = "Pxrxogxxrxamxoxwaxnixxexx xtxo xcxzexxscx ixnfxoxxrxxmxaxtxyxkxxixxx."
-print(f"4. p.replace('x', '')")
+print(f"4.", p.replace('x', ''))
 
-liczba1 = input("podaj pierwsza liczbe ")
-liczba2 = input("podaj druga liczbe ")
-print(f"5. Suma = {int(liczba1) + int(liczba2)}, Roznica = {int(liczba1) - int(liczba2)}, Potega 3 stopnia {liczba1} = {int(liczba1)**3}, Potega 3 stopnia {liczba2} = {int(liczba2)**3}, Reszta z dzielenia przez 7 liczby {liczba1} = {int(liczba1)%7}, Reszta z dzielenia przez 7 liczby {liczba2} = {int(liczba2)%7}")
+# 5. Calculate for 2 numbers given in the console:
+# a - sum
+# b - difference
+# c - power of three
+# d - the remainder after dividing by 7
+liczba1 = input("5. Input the first number: ")
+liczba2 = input("Input the second number: ")
+print(f"5. Sum = {int(liczba1) + int(liczba2)}, Difference = {int(liczba1) - int(liczba2)}, {liczba1} to the power of three = {int(liczba1)**3}, {liczba2} to the power of three = {int(liczba2)**3}, Remainder of {liczba1} after dividing by 7 = {int(liczba1)%7}, Remainder of {liczba2} after dividing by 7 = {int(liczba2)%7}")
 
+# 6. Print colored text
 A = '\033[1;33;46m'
 B = '\033[1;30;41m'
 C = '\033[1;31;44m'
 re = '\033[0m'
 print(C + "Programowanie" + re + " " + A + "jest" + re + " " + B + "SUPER" + re)
-
-rzymska1 = input("podaj 1 liczbe rzymska ")
-if rzymska1 == "I":
-    rzymska1 = 1
-elif rzymska1 == "V":
-    rzymska1 = 5
-elif rzymska1 == "X":
-    rzymska1 = 10
-elif rzymska1 == "L":
-    rzymska1 = 10
-elif rzymska1 == "C":
-    rzymska1 = 100
-elif rzymska1 == "D":
-    rzymska1 = 500
-elif rzymska1 == "M":
-    rzymska1 = 1000
-else:
-    print("to nie jest liczba rzymska")
-rzymska2 = input("podaj 2 liczbe rzymska ")
-if rzymska2 == "I":
-    rzymska2 = 1
-    print(rzymska1 + rzymska2)
-elif rzymska2 == "V":
-    rzymska2 = 5
-    print(rzymska1 + rzymska2)
-elif rzymska2 == "X":
-    rzymska2 = 10
-    print(rzymska1 + rzymska2)
-elif rzymska2 == "L":
-    rzymska2 = 10
-    print(rzymska1 + rzymska2)
-elif rzymska2 == "C":
-    rzymska2 = 100
-    print(rzymska1 + rzymska2)
-elif rzymska2 == "D":
-    rzymska2 = 500
-    print(rzymska1 + rzymska2)
-elif rzymska2 == "M":
-    rzymska2 = 1000
-    print(rzymska1 + rzymska2)
-else:
-    print("to nie jest liczba rzymska")
